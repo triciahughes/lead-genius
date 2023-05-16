98aa13cdd45d127aa706264524936101f079b279
Tricia Hughes <116763138+triciahughes@users.noreply.github.com>
2023-05-15 17:58:43 -0700

    mongoDB connected and working

--- server/server.js
+++ server/server.js
@@ -1,10 +1,22 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 
 app.get("/api", (req, res) => {
   res.json({ users: ["user1", "user2", "user3"] });
 });
 
-app.listen(3000, () => {
-  console.log("Server listening on port 3000");
-});
+mongoose
+  .connect(
+    "mongodb+srv://leadlocal:hellodatabase@leadgeniusdb.fiqr34g.mongodb.net/Lead-API?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("Connected to database!");
+    app.listen(3000, () => {
+      console.log("Server listening on port 3000");
+    });
+  })
+  .catch((error) => {
+    console.log("Connection failed!");
+    console.log(error);
+  });
